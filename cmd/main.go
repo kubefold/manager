1a8798df7457c39bde5910646d2a454d5f20da99
Mateusz Woźniak <matisiek11@gmail.com>
2025-05-03 20:19:58 +0200

    feat: implement SMS notification service using AWS SNS and update dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/kubefold/manager/internal/dto"
 	"github.com/kubefold/manager/internal/service"
@@ -13,6 +14,8 @@ func main() {
 	outputPath := os.Getenv("OUTPUT_PATH")
 	encodedInput := os.Getenv("ENCODED_INPUT")
 	bucket := os.Getenv("BUCKET")
+	notificationPhones := strings.Split(os.Getenv("NOTIFICATION_PHONES"), ",")
+	notificationMessage := os.Getenv("NOTIFICATION_MESSAGE")
 
 	if inputPath == "" {
 		logrus.Panicf("INPUT_PATH env var is required")
@@ -40,6 +43,15 @@ func main() {
 		err := services.Upload().UploadArtifacts(bucket)
 		if err != nil {
 			logrus.Fatalf("unable to upload artifacts: %v", err)
+		}
+		return
+	}
+
+	if len(notificationPhones) > 0 && notificationPhones[0] != "" && notificationMessage != "" {
+		logrus.Infof("Sending notification to %d phone numbers", len(notificationPhones))
+		err := services.Notification().SendNotification(notificationPhones, notificationMessage)
+		if err != nil {
+			logrus.Fatalf("unable to send notification: %v", err)
 		}
 		return
 	}
--- internal/service/notification.go
+++ internal/service/notification.go
@@ -1,9 +1,42 @@
 package service
 
-type NotificationService interface{}
+import (
+	"context"
 
-type notificationService struct{}
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+)
+
+type NotificationService interface {
+	SendNotification(phones []string, message string) error
+}
+
+type notificationService struct {
+	snsClient *sns.Client
+}
 
 func newNotificationService() NotificationService {
-	return &notificationService{}
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return nil
+	}
+	snsClient := sns.NewFromConfig(cfg)
+	return &notificationService{
+		snsClient: snsClient,
+	}
+}
+
+func (n *notificationService) SendNotification(phones []string, message string) error {
+	ctx := context.Background()
+	for _, phone := range phones {
+		_, err := n.snsClient.Publish(ctx, &sns.PublishInput{
+			Message:     aws.String(message),
+			PhoneNumber: aws.String(phone),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
